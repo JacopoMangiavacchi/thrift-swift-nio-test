4b21a4864be8e197ca76bce7642213b81546653a
J-Mangia <j.mangia@ibm.com>
2018-03-06 15:57:25 -0800

    Fallback to Swift 4.0

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url:"https://github.com/JacopoMangiavacchi/thrift-swift-nio.git", from: "0.0.0"),
-        .package(url: "https://github.com/JacopoMangiavacchi/Thrift-Swift.git", from: "1.0.0")
+        .package(url:"https://github.com/JacopoMangiavacchi/thrift-swift-nio.git", from: "0.0.2"),
+        .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # thrift-swift-nio-test
 Test Thrift Server made with Thrift-Swift-Nio
+
+    thrift --gen swift Test.thrift 
